f0f325bbbd513ee0f8dd31fa65809cf1454ee6e7
nicKondratiev <kondratievinikoloz@gmail.com>
2023-11-06 21:54:00 +0400

    Create CarsList.tsx component and add it to listings page

---
+++ app/listings/page.tsx
@@ -0,0 +1,9 @@
+import CarsList from "../../components/CarsList";
+
+export default function Listings() {
+  return (
+    <div>
+      <CarsList />
+    </div>
+  );
+}
--- components/CarsList.tsx
+++ components/CarsList.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function CarsList() {
-  return <div>CarsList</div>;
+  return (
+    <>
+      <div>
+        <Image src={""} alt="carImage" />
+        <div className="flex gap-2">
+          <h2 className="font-bold">Manufacturer Model</h2>
+          <h2 className="font-semibold text-gray-400">Year</h2>
+        </div>
+      </div>
+    </>
+  );
 }
