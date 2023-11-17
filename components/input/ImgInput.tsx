55ca242e88253334e6df364ca1a089a0231071de
nicKondratiev <kondratievinikoloz@gmail.com>
2023-11-17 15:58:41 +0400

    Update ImgInput.tsx so state is in sync with input's value

--- components/input/ImgInput.tsx
+++ components/input/ImgInput.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import useStore from "../../app/store";
+
 import InsertLinkIcon from "@mui/icons-material/InsertLink";
 
 export default function ImgInput() {
+  const store = useStore();
+
   return (
     <div className="flex h-[60px] w-full items-center gap-2 rounded-lg border border-dashed border-blue-500 px-2 ">
       <span className="rounded-lg bg-blue-50 p-1.5">
@@ -10,6 +14,7 @@ export default function ImgInput() {
       </span>
       <input
         type="text"
+        onChange={(e) => store.addImage(e.target.value)}
         placeholder="e.g. https://car-image/manufacturer"
         className="w-full text-sm font-medium text-gray-700 placeholder:font-normal focus:outline-none"
       />
