7c5d205e3431f2079f43d9310285276ce166f674
nicKondratiev <kondratievinikoloz@gmail.com>
2023-11-21 23:49:39 +0400

    Optimise zustand store by removing extra code

--- app/store.ts
+++ app/store.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-type InputFields = {
+export type InputFields = {
   manu: string;
   model: string;
   location: string;
@@ -14,24 +14,14 @@ type InputFields = {
   img: string;
 };
 
-// we create this types to avoid writing same types again and again
-type StringVoid = (val: string) => void;
+// type StringVoid = (val: string) => void;
 
 type Store = {
   inputFields: InputFields;
   isFormSubmitted: boolean;
   setFormSubmitted: () => void;
-  addManu: StringVoid;
-  addModel: StringVoid;
-  addLocation: StringVoid;
-  addFuelType: StringVoid;
-  addYear: StringVoid;
-  addPrice: StringVoid;
-  addCustom: StringVoid;
-  addWheel: StringVoid;
-  addTransmission: StringVoid;
-  addMileage: StringVoid;
-  addImage: StringVoid;
+  updateField: (fieldName: keyof InputFields, value: string) => void;
+  reset: () => void;
 };
 
 const useStore = create<Store>((set) => ({
@@ -50,95 +40,188 @@ const useStore = create<Store>((set) => ({
   },
   isFormSubmitted: false,
   setFormSubmitted: () => set({ isFormSubmitted: true }),
-  addManu: (manu) =>
+
+  updateField: (fieldName, value) =>
     set((state) => ({
       inputFields: {
         ...state.inputFields,
-        manu: manu,
-        model: "",
+        [fieldName]: value,
       },
     })),
 
-  addModel: (model) =>
+  reset: () => {
     set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        model: model,
-      },
-    })),
-
-  addLocation: (location) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        location: location,
-      },
-    })),
-
-  addCustom: (custom) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        custom: custom,
-      },
-    })),
-
-  addYear: (year) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        year: year,
-      },
-    })),
-
-  addPrice: (price) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        price: price,
-      },
-    })),
-
-  addFuelType: (fuelType) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        fuelType: fuelType,
-      },
-    })),
-
-  addWheel: (wheel) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        wheel: wheel,
-      },
-    })),
-
-  addTransmission: (transmission) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        transmission: transmission,
-      },
-    })),
-
-  addMileage: (mileage) =>
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        mileage: mileage,
-      },
-    })),
-
-  addImage: (img) => {
-    set((state) => ({
-      inputFields: {
-        ...state.inputFields,
-        img: img,
-      },
+      inputFields: Object.fromEntries(
+        Object.keys(state.inputFields).map((key) => [key, ""])
+      ) as InputFields,
+      isFormSubmitted: false,
     }));
   },
 }));
 
 export default useStore;
+
+// import { create } from "zustand";
+
+// type InputFields = {
+//   manu: string;
+//   model: string;
+//   location: string;
+//   custom: string;
+//   year: string;
+//   price: string;
+//   fuelType: string;
+//   wheel: string;
+//   transmission: string;
+//   mileage: string;
+//   img: string;
+// };
+
+// // we create this types to avoid writing same types again and again
+// type StringVoid = (val: string) => void;
+
+// type Store = {
+//   inputFields: InputFields;
+//   isFormSubmitted: boolean;
+//   setFormSubmitted: () => void;
+//   addManu: StringVoid;
+//   addModel: StringVoid;
+//   addLocation: StringVoid;
+//   addFuelType: StringVoid;
+//   addYear: StringVoid;
+//   addPrice: StringVoid;
+//   addCustom: StringVoid;
+//   addWheel: StringVoid;
+//   addTransmission: StringVoid;
+//   addMileage: StringVoid;
+//   addImage: StringVoid;
+//   reset: () => void;
+// };
+
+// const useStore = create<Store>((set) => ({
+//   inputFields: {
+//     manu: "",
+//     model: "",
+//     location: "",
+//     custom: "",
+//     year: "",
+//     price: "",
+//     fuelType: "",
+//     wheel: "",
+//     transmission: "",
+//     mileage: "",
+//     img: "",
+//   },
+//   isFormSubmitted: false,
+//   setFormSubmitted: () => set({ isFormSubmitted: true }),
+//   addManu: (manu) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         manu: manu,
+//         model: "",
+//       },
+//     })),
+
+//   addModel: (model) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         model: model,
+//       },
+//     })),
+
+//   addLocation: (location) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         location: location,
+//       },
+//     })),
+
+//   addCustom: (custom) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         custom: custom,
+//       },
+//     })),
+
+//   addYear: (year) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         year: year,
+//       },
+//     })),
+
+//   addPrice: (price) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         price: price,
+//       },
+//     })),
+
+//   addFuelType: (fuelType) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         fuelType: fuelType,
+//       },
+//     })),
+
+//   addWheel: (wheel) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         wheel: wheel,
+//       },
+//     })),
+
+//   addTransmission: (transmission) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         transmission: transmission,
+//       },
+//     })),
+
+//   addMileage: (mileage) =>
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         mileage: mileage,
+//       },
+//     })),
+
+//   addImage: (img) => {
+//     set((state) => ({
+//       inputFields: {
+//         ...state.inputFields,
+//         img: img,
+//       },
+//     }));
+//   },
+
+//   reset: () => {
+//     set((state) => ({
+//       inputFields: {
+//         manu: "",
+//         model: "",
+//         location: "",
+//         custom: "",
+//         year: "",
+//         price: "",
+//         fuelType: "",
+//         wheel: "",
+//         transmission: "",
+//         mileage: "",
+//         img: "",
+//       },
+//       isFormSubmitted: false,
+//     }));
+//   },
+// }));
+
+// export default useStore;
