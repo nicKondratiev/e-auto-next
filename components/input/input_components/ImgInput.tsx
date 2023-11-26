2451f192c1cc09098bb7c83851c6bae9b2b980fe
nicKondratiev <kondratievinikoloz@gmail.com>
2023-11-26 22:47:41 +0400

    Update code so image files can be uploaded instead of links

--- components/input/input_components/ImgInput.tsx
+++ components/input/input_components/ImgInput.tsx
@@ -1,28 +1,36 @@
 "use client";
 
+import { useRef } from "react";
 import useStore from "../../../app/store";
 
-import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 export default function ImgInput() {
   const { updateField, inputFields, isFormSubmitted } = useStore();
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div
+      onClick={() => inputRef?.current?.click()}
       className={`${
         isFormSubmitted && !inputFields.img
           ? "border-red-300"
           : "border-blue-500"
-      } flex h-[60px] w-full items-center gap-2 rounded-lg border border-dashed border-blue-500 px-2`}
+      } flex h-[80px] w-full cursor-pointer items-center gap-2 rounded-lg border border-dashed border-blue-500 bg-blue-100 px-2`}
     >
-      <span className="rounded-lg bg-blue-50 p-1.5">
-        <InsertLinkIcon className="text-blue-500" fontSize="medium" />
-      </span>
+      <div className="flex items-center text-sm font-semibold text-blue-500">
+        <span className="p-3">
+          <CameraAltIcon className="text-blue-500" fontSize="large" />
+        </span>
+        Upload Photo
+      </div>
       <input
-        type="text"
+        ref={inputRef}
+        type="file"
+        accept="image/jpeg,image/png,image/heif,image/heic"
         onChange={(e) => updateField("img", e.target.value)}
-        placeholder="e.g. https://car-image/manufacturer"
-        className="w-full text-sm font-medium text-gray-700 placeholder:font-normal focus:outline-none"
+        className="hidden"
       />
     </div>
   );
