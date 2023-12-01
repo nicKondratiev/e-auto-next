fc7282117c9633767f278b0b7d54aea7ba4f86c1
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-01 23:11:44 +0400

    Create handleChange function to update state in zustand store

--- components/input/AuthInput.tsx
+++ components/input/AuthInput.tsx
@@ -1,16 +1,41 @@
 "use client";
 
+import { useEffect } from "react";
+import useStore, { AuthFields } from "../../app/store";
+
 import "./styles.css";
 
-type AuthInput = {
+type AuthInputProps = {
+  fieldName: keyof AuthFields;
   type: string;
   placeholder: string;
 };
 
-export default function AuthInput({ type, placeholder }: AuthInput) {
+export default function AuthInput({
+  fieldName,
+  type,
+  placeholder,
+}: AuthInputProps) {
+  const { authFields, updateField, reset } = useStore();
+
+  console.log(authFields);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateField(fieldName, e.target.value);
+  };
+
+  useEffect(() => {
+    return () => reset();
+  }, [reset]);
+
   return (
     <div>
-      <input type={type} className="input" placeholder={placeholder} />
+      <input
+        onChange={handleChange}
+        type={type}
+        className="input"
+        placeholder={placeholder}
+      />
     </div>
   );
 }
