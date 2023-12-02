bb228caebd57034841c1014e1f2b9b7102e939e1
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-02 17:51:04 +0400

    Create reusable AuthButton.tsx component

---
+++ components/button/button_components/AuthButton.tsx
@@ -0,0 +1,18 @@
+type AuthButtonProps = {
+  buttonText: "Log in" | "Confirm";
+  handleSubmit: () => void;
+};
+
+export default function AuthButton({
+  buttonText,
+  handleSubmit,
+}: AuthButtonProps) {
+  return (
+    <button
+      onClick={handleSubmit}
+      className="h-14 w-full rounded-full bg-blue-500 font-light text-white"
+    >
+      {buttonText}
+    </button>
+  );
+}
