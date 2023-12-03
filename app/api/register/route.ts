af8c75e2003c5a4cfd7c36d258beea4e2df51fa1
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-03 14:14:56 +0400

    Add password hashing using bcrpypt

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,14 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "../../../models/User";
 import { connectMongoDB } from "../../../lib/mongodb";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: NextRequest) {
   await connectMongoDB();
 
   const { username, email, password } = await req.json();
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    await User.create({ username, email, password });
+    await User.create({ username, email, password: hashedPassword });
 
     return NextResponse.json(
       { message: "User registered successfuly" },
