d86694b12ec78a4993b0438705b12ff6329f52ee
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-04 16:47:41 +0400

    Create handleSubmit function for LoginButton

--- app/user/login/LoginButton.tsx
+++ app/user/login/LoginButton.tsx
@@ -1,16 +1,41 @@
 "use client";
 
 import useStore from "../../store";
+import { AuthFields } from "../../store";
 
 import AuthButton from "../../../components/button/button_components/AuthButton";
+import { signIn } from "next-auth/react";
+
+import { useRouter } from "next/navigation";
 
 export default function LoginButton() {
   const { authFields } = useStore();
 
-  const data = [authFields.email, authFields.password];
+  const router = useRouter();
 
-  const handleSubmit = () => {
-    console.log(data);
+  const data: Partial<AuthFields> = {
+    email: authFields.email,
+    password: authFields.password,
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        data,
+        redirect: false,
+      });
+
+      if (res?.error) {
+        console.log("something went wrong");
+        return;
+      }
+
+      router.replace("/");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return <AuthButton buttonText="Log in" handleSubmit={handleSubmit} />;
