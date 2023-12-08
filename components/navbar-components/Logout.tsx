0f97042dfe3479ff29af6d00210c859700387a2c
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-08 16:54:28 +0400

    Create handleClick function to handle sign out requests

--- components/navbar-components/Logout.tsx
+++ components/navbar-components/Logout.tsx
@@ -4,8 +4,14 @@ import { signOut } from "next-auth/react";
 import "./styles.css";
 
 export default function Logout() {
+  const handleClick = async () => {
+    await signOut({
+      callbackUrl: "/",
+    });
+  };
+
   return (
-    <button className="button" onClick={() => signOut()}>
+    <button className="button" onClick={handleClick}>
       Logout
     </button>
   );
