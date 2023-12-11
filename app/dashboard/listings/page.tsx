349ab0c4016f70b7c83ec8bcc091b1d1a8d01fc7
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-11 20:44:48 +0400

    Fetch and display data based on user's id

--- app/dashboard/listings/page.tsx
+++ app/dashboard/listings/page.tsx
@@ -1,13 +1,33 @@
 import CarsList from "../../../components/CarsList";
+import { InputFields } from "../../store";
 
-export default function Listings() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+
+export type ListingType = InputFields;
+
+async function getCarListngs() {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?._id;
+
+  const res = await fetch(`http://localhost:3000/api/cars/${userId}`);
+
+  if (!res.ok) {
+    throw new Error("failed to fetch car listings");
+  }
+
+  return await res.json();
+}
+
+export default async function Listings() {
+  const res = await getCarListngs();
+  const carListings = res.data;
+
   return (
     <div className="flex w-[800px] flex-col gap-4">
-      <CarsList />
-      <CarsList />
-      <CarsList />
-      <CarsList />
-      <CarsList />
+      {carListings.map((listing: ListingType, index: number) => (
+        <CarsList key={index} carListing={listing} />
+      ))}
     </div>
   );
 }
--- components/CarsList.tsx
+++ components/CarsList.tsx
@@ -3,47 +3,54 @@ import Link from "next/link";
 
 import EditIcon from "@mui/icons-material/Edit";
 import RemoveBtn from "./RemoveBtn";
+import { ListingType } from "../app/dashboard/listings/page";
 
-export default function CarsList() {
+export default function CarsList({ carListing }: { carListing: ListingType }) {
   return (
-    <>
-      <div className="flex h-48 gap-4 rounded-lg bg-white p-4">
-        <div className="w-2/6 rounded-lg bg-gray-500">
-          <Image src={""} alt="carImage" />
+    <div className="flex h-48 gap-4 rounded-lg bg-white p-4">
+      <div className="w-2/6 overflow-hidden rounded-lg bg-gray-500">
+        <Image
+          className="h-full w-full"
+          src={`${carListing.img}`}
+          alt="carImage"
+          width={100}
+          height={100}
+        />
+      </div>
+
+      <div className="flex w-3/6 flex-col justify-between gap-2">
+        <div className="flex gap-2">
+          <h2 className="font-bold">
+            {carListing.manu} {carListing.model}
+          </h2>
+          <h2 className="font-medium text-gray-400">{carListing.year}</h2>
         </div>
 
-        <div className="flex w-3/6 flex-col justify-between gap-2">
-          <div className="flex gap-2">
-            <h2 className="font-bold">Manufacturer Model</h2>
-            <h2 className="font-medium text-gray-400">Year</h2>
-          </div>
-
-          <div className="grid grid-cols-2 grid-rows-2 gap-y-2">
-            <h3>Fuel Type</h3>
-            <h3>Transmition</h3>
-            <h3>Miles</h3>
-            <h3>Steering</h3>
-          </div>
-
-          <div>
-            <h2>Priority</h2>
-          </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-y-2">
+          <h3>{carListing.fuelType}</h3>
+          <h3>{carListing.transmission}</h3>
+          <h3>{carListing.mileage}</h3>
+          <h3>{carListing.wheel}</h3>
         </div>
 
-        <div className="flex flex-col items-end justify-between gap-2">
-          <div className="flex gap-3">
-            <h3>Location</h3>
-            <h3>Clearence</h3>
-          </div>
-          <h1>Price $</h1>
-          <div className="flex gap-2">
-            <RemoveBtn />
-            <Link href={"/editListing/1"}>
-              <EditIcon fontSize="medium" />
-            </Link>
-          </div>
+        <div>
+          <h2>Priority</h2>
         </div>
       </div>
-    </>
+
+      <div className="flex flex-col items-end justify-between gap-2">
+        <div className="flex gap-3">
+          <h3>{carListing.location}</h3>
+          <h3>{carListing.custom}</h3>
+        </div>
+        <h1>{carListing.price} $</h1>
+        <div className="flex gap-2">
+          <RemoveBtn />
+          <Link href={"/editListing/1"}>
+            <EditIcon fontSize="medium" />
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
