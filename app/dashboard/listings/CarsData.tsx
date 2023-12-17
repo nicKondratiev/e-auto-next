c7b09d48d090978fb11ebd2f07ae8a2cd070532e
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-17 17:18:07 +0400

    Change data fetching logic so data is fetched based on page query param

--- app/dashboard/listings/CarsData.tsx
+++ app/dashboard/listings/CarsData.tsx
@@ -5,34 +5,37 @@ import { InputFields } from "../../store";
 
 export type ListingType = InputFields & { _id: string };
 
-async function getCarListngs() {
-  const session = await getServerSession(authOptions);
-  const userId = session?.user?._id;
-
-  const res = await fetch(`http://localhost:3000/api/cars/${userId}`, {
-    method: "GET",
-  });
-
-  if (!res.ok) {
-    throw new Error("failed to fetch car listings");
-  }
-
-  return await res.json();
-}
-
 async function handleDelete(carId: string) {
   "use server";
-  await fetch(`http://localhost:3000/api/cars/${carId}`, {
+  await fetch(`http://localhost:3000/api/cars?carId=${carId}`, {
     method: "DELETE",
   });
 }
 
-export default async function CarsData() {
+export default async function CarsData({ page }: { page: number }) {
+  async function getCarListngs() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?._id;
+
+    const res = await fetch(
+      `http://localhost:3000/api/cars?userId=${userId}&page=${page}`,
+      {
+        method: "GET",
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("failed to fetch car listings");
+    }
+
+    return await res.json();
+  }
+
   const res = await getCarListngs();
   const carListings = res.data;
 
   return (
-    <div className="flex h-full w-[800px] flex-col gap-4">
+    <div className="grid h-full w-[800px] grid-cols-1 grid-rows-3 flex-col items-center">
       {carListings.reverse().map((listing: ListingType, index: number) => (
         <CarsList
           handleDelete={handleDelete}
