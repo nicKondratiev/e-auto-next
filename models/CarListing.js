50137e13330af961e056533309c0b525f70d2ce7
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-19 19:43:56 +0400

    Add timestamps to CarListing schema

--- models/CarListing.js
+++ models/CarListing.js
@@ -5,24 +5,31 @@ const RequiredString = {
   required: true,
 };
 
-const CarListingSchema = new mongoose.Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const schemaOptions = {
+  timestamps: { createdAt: "createdAt" },
+};
+
+const CarListingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    manu: RequiredString,
+    model: RequiredString,
+    location: RequiredString,
+    custom: RequiredString,
+    year: RequiredString,
+    price: RequiredString,
+    fuelType: RequiredString,
+    wheel: RequiredString,
+    transmission: RequiredString,
+    mileage: RequiredString,
+    img: RequiredString,
   },
-  manu: RequiredString,
-  model: RequiredString,
-  location: RequiredString,
-  custom: RequiredString,
-  year: RequiredString,
-  price: RequiredString,
-  fuelType: RequiredString,
-  wheel: RequiredString,
-  transmission: RequiredString,
-  mileage: RequiredString,
-  img: RequiredString,
-});
+  schemaOptions
+);
 
 const CarListing =
   mongoose.models.CarListing || mongoose.model("CarListing", CarListingSchema);
