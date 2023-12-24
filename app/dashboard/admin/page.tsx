2ba1fb9ea8ddd53b4ba86380c200a0356f581642
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-24 20:40:20 +0400

    Create client-side Table component to display all the users

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,11 +1,18 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 import { notFound } from "next/navigation";
+import UserTable from "../../../components/UserTable";
 
 export default async function AdminPage() {
   const session = await getServerSession(authOptions);
 
   if (session.user.role !== "ADMIN") return notFound();
 
-  return <div>AdminPage</div>;
+  return (
+    <div>
+      <div className="container">
+        <UserTable />
+      </div>
+    </div>
+  );
 }
