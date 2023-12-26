f40087dbfd1556c514b4c42ff022559e13c10665
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-26 20:15:12 +0400

    Add Pagination component to the Table

--- components/UserTable.tsx
+++ components/UserTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useMemo, useState } from "react";
 import {
   Table,
   TableHeader,
@@ -8,23 +8,46 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  getKeyValue,
+  Pagination,
 } from "@nextui-org/react";
 import { columns, renderCell } from "../app/dashboard/admin/columns";
 import { User } from "../app/dashboard/admin/page";
 
 export default function UserTable({ users }: { users: User[] }) {
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 5;
+
+  const pages = Math.ceil(users.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return users.slice(start, end);
+  }, [page, users]);
+
   return (
-    <Table aria-label="Example table with dynamic content">
+    <Table
+      aria-label="Example table with dynamic content"
+      bottomContent={
+        <div className="flex justify-center">
+          <Pagination
+            showControls
+            total={pages}
+            page={page}
+            onChange={(page) => setPage(page)}
+          />
+        </div>
+      }
+    >
       <TableHeader columns={columns}>
         {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
-      <TableBody items={users} emptyContent={"No users to display."}>
+      <TableBody items={items} emptyContent={"No users to display."}>
         {(item) => (
           <TableRow key={item._id}>
             {(columnKey) => (
               <TableCell>{renderCell(item, columnKey)}</TableCell>
-              // <TableCell>{getKeyValue(item, columnKey)}</TableCell>
             )}
           </TableRow>
         )}
