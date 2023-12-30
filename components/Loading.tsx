06474cd7cc58d1482515ea4405145b99dd2da41a
nicKondratiev <kondratievinikoloz@gmail.com>
2023-12-30 22:40:31 +0400

    Create reusable loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-full w-screen items-center justify-center">
+      <span className="text-xl font-semibold text-black">Loading...</span>
+    </div>
+  );
+}
