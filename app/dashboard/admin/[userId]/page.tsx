28113a4ca4df8d6ae1dd8fca7ad0e977bca089aa
nicKondratiev <kondratievinikoloz@gmail.com>
2024-01-02 13:03:24 +0400

    Generate data statically

--- app/dashboard/admin/[userId]/page.tsx
+++ app/dashboard/admin/[userId]/page.tsx
@@ -1,5 +1,28 @@
-export default function User({ params }: { params: { userId: string } }) {
+import { User } from "../page";
+
+export async function generateStaticParams() {
+  const users = await fetch("http://localhost:3000/api/users", {
+    method: "GET",
+  }).then((res) => res.json());
+
+  return users.map((user: any) => ({
+    userId: user._id,
+  }));
+}
+
+const getUserListings = async (userId: string) => {
+  const data = await fetch(`http://localhost:3000/api/cars?userId=${userId}`, {
+    method: "GET",
+  }).then((res) => res.json());
+
+  return data;
+};
+
+export default async function User({ params }: { params: { userId: string } }) {
   const userId = params.userId;
+  const userListings = await getUserListings(userId);
+
+  console.log(userListings);
 
   return <div>User Id: {userId}</div>;
 }
