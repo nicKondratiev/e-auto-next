be78d69b044d284c7716151403dd10f321dc5dec
nicKondratiev <kondratievinikoloz@gmail.com>
2024-01-02 13:15:12 +0400

    Update heights of Loading and layout components

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex h-full w-screen items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <span className="text-xl font-semibold text-black">Loading...</span>
     </div>
   );
