669fa30c82a310da099b46704c93fbc8db01d11b
nicKondratiev <kondratievinikoloz@gmail.com>
2024-01-07 19:09:56 +0400

    Apply dropdown-like animation whenever edit button is clicked

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,18 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { User as UserType } from "../app/dashboard/admin/page";
 import { User } from "@nextui-org/react";
 import { EditIcon } from "./icons";
 import AdminActions from "./AdminActions";
-
-const getUser = async (id: string) => {
-  const data = await fetch(`http://localhost:3000/api/users?userId=${id}`, {
-    method: "GET",
-  });
-
-  return data.json();
-};
 
 type UserInterface = UserType & {
   createdAt: string;
@@ -20,72 +12,68 @@ type UserInterface = UserType & {
   banExpirationDate: null | string;
 };
 
-export default function UserCard({ id }: { id: string }) {
-  const [user, setUser] = useState<UserInterface | null>(null);
+export default function UserCard({ user }: { user: UserInterface }) {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const userData = await getUser(id);
-
-        setUser(userData);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchData();
-  }, [id]);
-
-  console.log(isModalOpen);
+  console.log(user);
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="h-1/3 w-[300px] rounded-xl bg-white px-4 py-2 shadow-small">
-        {user?._id && (
-          <div className="flex h-full flex-col items-start justify-between">
-            <div className="flex w-full items-center justify-between">
-              <User name={user.username} />
-              <button className="text-lg text-gray-700 duration-150 hover:scale-110 hover:text-black">
-                <EditIcon onClick={() => setIsModalOpen(!isModalOpen)} />
-              </button>
-            </div>
+    <div className="h-full">
+      <div
+        className={`flex w-[300px] flex-col overflow-hidden rounded-xl bg-white px-4 py-2 shadow-small duration-300 ${
+          isModalOpen ? "h-full flex-1" : "h-1/3"
+        }`}
+      >
+        <div className="flex shrink-0 flex-col gap-4">
+          <div>
+            {user?._id && (
+              <div className="flex h-1/3 flex-col items-start">
+                <div className="flex w-full items-center justify-between">
+                  <User name={user.username} />
+                  <button className="text-lg text-gray-700 duration-150 hover:scale-110 hover:text-black">
+                    <EditIcon onClick={() => setIsModalOpen(!isModalOpen)} />
+                  </button>
+                </div>
 
-            <div className="flex flex-col gap-0.5">
-              <span className="flex gap-2 text-xs font-semibold">
-                Creation date:
-                <p className="font-normal">
-                  {new Date(user.createdAt).toLocaleString()}
-                </p>
-              </span>
+                <div className="flex flex-col gap-0.5">
+                  <span className="text-sm font-semibold">Information</span>
+                  <span className="flex gap-2 text-xs font-semibold">
+                    Creation date:
+                    <p className="font-normal">
+                      {new Date(user.createdAt).toLocaleString()}
+                    </p>
+                  </span>
+                  <span className="flex gap-2 text-xs font-semibold">
+                    User ID:
+                    <p className="font-normal">{user._id}</p>
+                  </span>
+                  <span className="flex gap-2 text-xs font-semibold">
+                    IsBanned:
+                    <p className="font-normal">{String(user.isBanned)}</p>
+                  </span>
+                  <span className="flex gap-2 text-xs font-semibold">
+                    Ban exp date:
+                    <p className="font-normal">
+                      {String(user.banExpirationDate)}
+                    </p>
+                  </span>
+                </div>
 
-              <span className="flex gap-2 text-xs font-semibold">
-                User ID:
-                <p className="font-normal">{user._id}</p>
-              </span>
-
-              <span className="flex gap-2 text-xs font-semibold">
-                IsBanned:
-                <p className="font-normal">{String(user.isBanned)}</p>
-              </span>
-
-              <span className="flex gap-2 text-xs font-semibold">
-                Ban exp date:
-                <p className="font-normal">{String(user.banExpirationDate)}</p>
-              </span>
-            </div>
-
-            <div>
-              <span className="flex gap-2 text-xs font-semibold">
-                Total Listings:
-                <p className="font-normal">2</p>
-              </span>
-            </div>
+                <div>
+                  <span className="flex gap-2 text-xs font-semibold">
+                    Total Listings:
+                    <p className="font-normal">2</p>
+                  </span>
+                </div>
+              </div>
+            )}
           </div>
-        )}
+
+          <div>
+            <AdminActions isBanned={user?.isBanned!} />
+          </div>
+        </div>
       </div>
-      {isModalOpen && <AdminActions isBanned={user?.isBanned!} />}
     </div>
   );
 }
