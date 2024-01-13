ae67f20b425034db451273a5c57f7c18858fedb9
nicKondratiev <kondratievinikoloz@gmail.com>
2024-01-13 21:46:25 +0400

    Update UserCard.tsx component's styling

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { User } from "@nextui-org/react";
+import { Card, User } from "@nextui-org/react";
 import { EditIcon } from "./icons";
 import AdminActions from "./AdminActions";
 import { UserInterface } from "../app/dashboard/admin/page";
@@ -14,24 +14,27 @@ export default function UserCard({ user }: { user: UserInterface }) {
   return (
     <div className="h-full">
       <div
-        className={`flex w-[400px] flex-col overflow-hidden rounded-xl bg-white px-4 py-2 shadow-small duration-300 ${
+        className={`box-border flex w-[400px] flex-col overflow-hidden rounded-xl bg-white px-4 py-2 shadow-small duration-300 ${
           isModalOpen ? "h-full flex-1" : "h-1/3"
         }`}
       >
         {user?._id && (
           <div className="flex h-full shrink-0 flex-col gap-2">
-            <div>
-              <div className="flex h-1/3 flex-col items-start gap-2">
+            <div className="">
+              <div className="flex flex-col items-start">
                 <div className="flex w-full items-center justify-between">
                   <User name={user.username} />
-                  <button className="text-lg text-gray-700 duration-150 hover:scale-110 hover:text-black">
-                    <EditIcon onClick={() => setIsModalOpen(!isModalOpen)} />
+                  <button
+                    onClick={() => setIsModalOpen(!isModalOpen)}
+                    className="rounded-lg bg-blue-500 p-1 text-lg text-gray-100 duration-150 hover:scale-110 hover:text-white"
+                  >
+                    <EditIcon />
                   </button>
                 </div>
 
-                <div className="flex w-full flex-col">
+                <div className="flex w-full flex-col gap-2">
                   <span className="text-sm font-semibold">Information</span>
-                  <div className="w-full rounded-lg px-2 shadow-none">
+                  <Card className="rounded-lg px-2 py-1">
                     <span className={spanStyling}>
                       Creation date:
                       <p className="font-normal">
@@ -39,11 +42,7 @@ export default function UserCard({ user }: { user: UserInterface }) {
                       </p>
                     </span>
                     <span className={spanStyling}>
-                      User ID:
-                      <p className="font-normal">{user._id}</p>
-                    </span>
-                    <span className={spanStyling}>
-                      IsBanned:
+                      Is Banned:
                       <p className="font-normal">{String(user.isBanned)}</p>
                     </span>
                     <span className={spanStyling}>
@@ -56,7 +55,7 @@ export default function UserCard({ user }: { user: UserInterface }) {
                       Total Listings:
                       <p className="font-normal">2</p>
                     </span>
-                  </div>
+                  </Card>
                 </div>
               </div>
             </div>
